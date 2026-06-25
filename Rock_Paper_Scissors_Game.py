200a82e3bcb6d300bbea76b808417cb0e80ec49c
krupalipatel-26 <krupalipatel5529@gmail.com>
2026-06-25 11:55:37 +0530

    Add files via upload

---
+++ Rock_Paper_Scissors_Game.py
@@ -0,0 +1,50 @@
+import random
+
+print("===== Rock Paper Scissors Game =====")
+
+user_score = 0
+computer_score = 0
+
+choices = ["rock", "paper", "scissors"]
+
+while True:
+
+    user_choice = input("\nEnter Rock, Paper, or Scissors: ").lower()
+
+    if user_choice not in choices:
+        print("Invalid choice! Please try again.")
+        continue
+
+    computer_choice = random.choice(choices)
+
+    print(f"\nYou chose: {user_choice}")
+    print(f"Computer chose: {computer_choice}")
+
+    # Check winner
+    if user_choice == computer_choice:
+        print("It's a Tie!")
+
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        print("🎉 You Win!")
+        user_score += 1
+
+    else:
+        print("💻 Computer Wins!")
+        computer_score += 1
+
+    print(f"\nScore:")
+    print(f"You: {user_score}")
+    print(f"Computer: {computer_score}")
+
+    play_again = input("\nPlay Again? (yes/no): ").lower()
+
+    if play_again != "yes":
+        print("\nFinal Score")
+        print(f"You: {user_score}")
+        print(f"Computer: {computer_score}")
+        print("Thank you for playing!")
+        break
